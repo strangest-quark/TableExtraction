5ced03fa74321fde378f0776940aa1f1259741c0
Anudeep <anudeep.j06@gmail.com>
2018-12-01 00:36:45 +0530

    Update pdf_table_read.py

--- PDF Module/pdf_table_read.py
+++ PDF Module/pdf_table_read.py
@@ -1,66 +1,38 @@
-##from tabula import *
-##
-##convert_into("010_n.pdf", "test_n.csv", output = "csv")
-###tabula.convert_into("table.pdf", "output.csv", output_format="csv")
-###print df
-##
-
 from tabula import *
 import pyPdf,os, time
 import pandas as pd
 
-filename = "008.pdf"
+filename = raw_input("Enter the pdf you want to extract the table --> ") + ".pdf"
 pdf = pyPdf.PdfFileReader(open(filename, "rb"))
 pag_no = pdf.getNumPages()
 
-#-------------------------------- Page Segmentation ---------------------------------#
 for i in range(0,pag_no):
     pg = pdf.getPage(i)
     
     writer = pyPdf.PdfFileWriter()
 
     writer.addPage(pg)
-    NewPDFfilename = "allTables"+str(i)+".pdf"
+    NewPDFfilename = "Page_"+str(i)+".pdf"
     with open(NewPDFfilename, "wb") as outputStream: 
         writer.write(outputStream)
 
 time.sleep(0.2)
 
-
 for i in range(0,pag_no):
-    convert_into('allTables'+str(i)+'.pdf', 'test_'+str(i)+'.csv', output_format = 'CSV')
+    convert_into('Page_'+str(i)+'.pdf', 'result_'+str(i)+'.csv', output_format = 'CSV')
 
 for i in range(0,pag_no):
     try:
-        df = pd.read_csv("test_"+str(i)+".csv")
+        df = pd.read_csv("result_"+str(i)+".csv")
         if(df.empty):
             print "yes"
         else:
-            print "Table found in -----> PAGE"+str(i+1)+" and stored in -----> test_"+str(i)+".csv"
+            print "Table found in -----> PAGE"+str(i+1)+" and stored in -----> result_"+str(i)+".csv"
     except:
-        os.remove(r'C:\Users\Anudeep\Desktop\table_proj\allTables'+str(i)+'.pdf')
-        os.remove(r'C:\Users\Anudeep\Desktop\table_proj\test_'+str(i)+'.csv')
+        os.remove('/home/Username/Desktop/TableExtraction/Page_'+str(i)+'.pdf')
+        os.remove('/home/Username/Desktop/TableExtraction/result_'+str(i)+'.csv')
         pass
 
 
-
-
-
-
-
-
-
-
-
-##------Programme for extracting the data and writing it into another pdf----#
-##pg3 = pdf.getPage(2)
-##
-##writer = pyPdf.PdfFileWriter()
-##
-##writer.addPage(pg6)
-##
-##NewPDFfilename = "allTables.pdf"
-##with open(NewPDFfilename, "wb") as outputStream: 
-##    writer.write(outputStream) 
 
 
